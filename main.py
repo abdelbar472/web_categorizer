823869cffaab46f85aa4191ee5e80db09c1a72d8
abdelbar472 <621041@oi.edu.eg>
2024-03-12 22:32:16 +0200

    url fix

--- main.py
+++ main.py
@@ -50,33 +50,77 @@ class CategoryResource(Resource):
             return jsonify({'error': 'Space not found'}), 404
 #subcategory
 class SubcategoryResource(Resource):
-    def get(self,space_name=None ,category_name=None, parent_subcategory_name=None):
-        if space_name:
-            space = space.query.filter_by(space_name=space_name).first()
-            if space:
-                if category_name:
-                    category = Category.query.filter_by(category_name=category_name).first()
-                    if category:
-                        subcategories = Subcategory.query.filter_by(category_id=category.id).all()
-                        ps = Subcategory.query.filter_by(subcategory_name=parent_subcategory_name).first()
-                        if subcategories and ps is None:
-                            return jsonify([{'id': subcategory.id, 'subcategory_name': subcategory.subcategory_name, 'category_id': subcategory.category_id} for subcategory in subcategories])
-                        elif subcategories and ps:
-                            subcategories = Subcategory.query.filter_by(parent_subcategory_id=ps.id).all()
-                            return jsonify([{'id': subcategory.id, 'subcategory_name': subcategory.subcategory_name, 'category_id': subcategory.category_id} for subcategory in subcategories])
+        def get(self, space_name=None, category_name=None, parent_subcategory_name=None):
+            if space_name:
+                space = Space.query.filter_by(space_name=space_name).first()
+                if space:
+                    if category_name:
+                        category = Category.query.filter_by(category_name=category_name).first()
+                        if category:
+                            subcategories = Subcategory.query.filter_by(category_id=category.id,
+                                                                        parent_subcategory_id=None).all()
+                            return jsonify([{'id': subcategory.id, 'subcategory_name': subcategory.subcategory_name,
+                                             'category_id': subcategory.category_id} for subcategory in subcategories])
+                        else:
+                            if parent_subcategory_name:
+                                parent_subcategory = Subcategory.query.filter_by(subcategory_name=parent_subcategory_name).first()
+                                if parent_subcategory:
+                                    subcategories = Subcategory.query.filter_by(parent_subcategory_id=parent_subcategory.id).all()
+                                    return jsonify([{'id': subcategory.id,
+                                                     'subcategory_name': subcategory.subcategory_name,
+                                                     'category_id': subcategory.category_id,
+                                                     'parent_subcategory_name': parent_subcategory.subcategory_name} for
+                                                    subcategory in subcategories])
+                                else:
+                                    return jsonify({'message': 'Parent subcategory not found'}), 404
+                            else:
+                                subcategories = Subcategory.query.filter_by(category_id=category.id).all()
+                                return jsonify([{'id': subcategory.id, 'subcategory_name': subcategory.subcategory_name,
+                                                 'category_id': subcategory.category_id} for subcategory in
+                                                subcategories])
+                    else:
+                        categories = Category.query.filter_by(space_id=space.id).all()
+                        return jsonify(
+                            [{'id': category.id, 'category_name': category.category_name, 'space_id': category.space_id}
+                             for category in categories])
                 else:
-                    categories = Category.query.filter_by(space_id=space.id).all()
-                    return jsonify([{'id': category.id, 'category_name': category.category_name, 'space_id': category.space_id} for category in categories])
+                    return jsonify({'message': 'Space not found'}), 404
 
+        def post(self, space_name, category_name, parent_subcategory_name=None):
+            parser = reqparse.RequestParser()
+            parser.add_argument('subcategory_name', type=str, required=True)
+            args = parser.parse_args()
+            space = Space.query.filter_by(space_name=space_name).first()
+            if space:
+                category = Category.query.filter_by(category_name=category_name).first()
+                if category:
+                    if parent_subcategory_name:
+                        parent_subcategory = Subcategory.query.filter_by(subcategory_name=parent_subcategory_name).first()
+                        if parent_subcategory:
+                            subcategory = Subcategory(subcategory_name=args['subcategory_name'], category_id=category.id,
+                                                      parent_subcategory_id=parent_subcategory.id)
+                            db.session.add(subcategory)
+                            db.session.commit()
+                            return jsonify({'id': subcategory.id, 'subcategory_name': subcategory.subcategory_name,
+                                            'category_id': subcategory.category_id,
+                                            'parent_subcategory_name': parent_subcategory.subcategory_name})
+                        else:
+                            return jsonify({'message': 'Parent subcategory not found'}), 404
+                    else:
+                        subcategory = Subcategory(subcategory_name=args['subcategory_name'], category_id=category.id)
+                        db.session.add(subcategory)
+                        db.session.commit()
+                        return jsonify({'id': subcategory.id, 'subcategory_name': subcategory.subcategory_name,
+                                        'category_id': subcategory.category_id})
+                else:
+                    return jsonify({'message': 'Category not found'}), 404
             else:
                 return jsonify({'message': 'Space not found'}), 404
 
 
-
-
 api.add_resource(SpaceResource, '/space', '/space/<string:space_name>')
 api.add_resource(CategoryResource, '/space/<string:space_name>/category', '/category')
-api.add_resource(SubcategoryResource,'/space/<string:space_name>/category/<string:category_name>/subcategory','/subcategory','/space/<string:space_name>/category/<string:category_name>/subcategory/<string:parent_subcategory_name>', '/subcategory/<string:parent_subcategory_name>')
+api.add_resource(SubcategoryResource,'/space/<string:space_name>/category/<string:category_name>/subcategory','/space/<string:space_name>/category/<string:category_name>/subcategory/<string:parent_subcategory_name>')
 
 
 
@@ -84,3 +128,27 @@ api.add_resource(SubcategoryResource,'/space/<string:space_name>/category/<strin
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    '''class SubcategoryResource(Resource):
+    def get(self, space_name=None, category_name=None, parent_subcategory_name=None):
+        if space_name:
+            space = Space.query.filter_by(space_name=space_name).first()
+            if space:
+                if category_name:
+                    category = Category.query.filter_by(category_name=category_name).first()
+                    if category:
+                        if parent_subcategory_name:
+                            parent_subcategory = Subcategory.query.filter_by(subcategory_name=parent_subcategory_name).first()
+                            if parent_subcategory:
+                                subcategories = Subcategory.query.filter_by(parent_subcategory_id=parent_subcategory.id).all()
+                                return jsonify([{'id': subcategory.id, 'subcategory_name': subcategory.subcategory_name, 'category_id': subcategory.category_id, 'parent_subcategory_name': parent_subcategory.subcategory_name} for subcategory in subcategories])
+                            else:
+                                return jsonify({'message': 'Parent subcategory not found'}), 404
+                        else:
+                            subcategories = Subcategory.query.filter_by(category_id=category.id).all()
+                            return jsonify([{'id': subcategory.id, 'subcategory_name': subcategory.subcategory_name, 'category_id': subcategory.category_id} for subcategory in subcategories])
+                else:
+                    categories = Category.query.filter_by(space_id=space.id).all()
+                    return jsonify([{'id': category.id, 'category_name': category.category_name, 'space_id': category.space_id} for category in categories])
+            else:
+                return jsonify({'message': 'Space not found'}), 404'''
--- url.py
+++ url.py
@@ -1,19 +1,123 @@
 from main import *
 class UrlResource(Resource):
-    def get(self):
-        urls = Url.query.all()
-        return [url.__dict__ for url in urls]
-    def post(self):
+    def get(self, space_name=None, category_name=None, subcategory_name=None):
+        if space_name:
+            space = Space.query.filter_by(space_name=space_name).first()
+            if space:
+                if category_name:
+                    category = Category.query.filter_by(category_name=category_name).first()
+                    if category:
+                        if subcategory_name:
+                            subcategory = Subcategory.query.filter_by(subcategory_name=subcategory_name).first()
+                            if subcategory:
+                                urls = Url.query.filter_by(subcategory_id=subcategory.id).all()
+                                return jsonify([{'id': url.id, 'name': url.name, 'link': url.link, 'image': url.image, 'category_id': url.category_id, 'category_name': url.category_name, 'subcategory_id': url.subcategory_id, 'subcategory_name': url.subcategory_name} for url in urls])
+                            else:
+                                return jsonify({'message': 'Subcategory not found'}), 404
+                        else:
+                            urls = Url.query.filter_by(category_id=category.id).all()
+                            return jsonify([{'id': url.id, 'name': url.name, 'link': url.link, 'image': url.image, 'category_id': url.category_id, 'category_name': url.category_name, 'subcategory_id': url.subcategory_id, 'subcategory_name': url.subcategory_name} for url in urls])
+                else:
+                    categories = Category.query.filter_by(space_id=space.id).all()
+                    return jsonify([{'id': category.id, 'category_name': category.category_name, 'space_id': category.space_id} for category in categories])
+            else:
+                return jsonify({'message': 'Space not found'}), 404
+        else:
+            urls = Url.query.all()
+            return jsonify([{'id': url.id, 'name': url.name, 'link': url.link, 'image': url.image, 'category_id': url.category_id, 'category_name': url.category_name, 'subcategory_id': url.subcategory_id, 'subcategory_name': url.subcategory_name} for url in urls])
+
+    def post(self, space_name, category_name, subcategory_name=None):
         parser = reqparse.RequestParser()
-        parser.add_argument('name', type=str, required=True)
         parser.add_argument('link', type=str, required=True)
-        parser.add_argument('image', type=str, required=False)
-        parser.add_argument('category_id', type=int, required=True)
-        parser.add_argument('category_name', type=str, required=True)
-        parser.add_argument('subcategory_id', type=int, required=True)
-        parser.add_argument('subcategory_name', type=str, required=True)
         args = parser.parse_args()
-        url = Url(name=args['name'], link=args['link'], image=args['image'], category_id=args['category_id'], category_name=args['category_name'], subcategory_id=args['subcategory_id'], subcategory_name=args['subcategory_name'])
-        db.session.add(url)
-        db.session.commit()
-        return url.__dict__
+
+        def get_website_name(url):
+            """
+            Get the website name from the given URL.
+
+            Args:
+                url (str): The URL of the website.
+
+            Returns:
+                str: The website name.
+            """
+            parsed_url = urlparse(url)
+            if parsed_url.netloc:
+                return parsed_url.netloc.replace(".com", "")
+            return "Invalid URL"
+
+        name = get_website_name(args['link'])
+
+        def show_image(image_url):
+            """
+            Display the image from the given URL.
+
+            Args:
+                image_url (str): The URL of the image.
+            """
+            try:
+                # Make an HTTP request to the URL with a timeout of 5 seconds
+                response = requests.get(image_url, timeout=5)
+
+                # Check if the request was successful (status code 200)
+                if response.status_code == 200:
+                    # Read the image content from the response
+                    image_content = response.content
+
+                    # Create a PIL Image object from the image content
+                    image = Image.open(BytesIO(image_content))
+
+                    # Display the image
+                    # image.show()
+                else:
+                    print(f"Failed to download image. Status code: {response.status_code}")
+            except requests.exceptions.RequestException as e:
+                print(f"Error: {e}")
+
+        space = Space.query.filter_by(space_name=space_name).first()
+        if space:
+            category = Category.query.filter_by(category_name=category_name, space_id=space.id).first()
+            if category:
+                new_url = Url(name=name, link=args['link'], category_id=category.id,
+                              category_name=category.category_name)
+                db.session.add(new_url)
+                db.session.commit()
+
+                return jsonify({'message': 'URL added',
+                                'url': {'id': new_url.id, 'name': new_url.name, 'link': new_url.link,
+                                        'category_id': new_url.category_id,
+                                        'category_name': new_url.category_name}}), 201
+            else:
+                return jsonify({'message': 'Category not found'}), 404
+        else:
+            return jsonify({'message': 'Space not found'}), 404
+
+
+
+'''
+def show_image(image_url):
+    """
+    Display the image from the given URL.
+    
+    Args:
+        image_url (str): The URL of the image.
+    """
+    try:
+        # Make an HTTP request to the URL with a timeout of 5 seconds
+        response = requests.get(image_url, timeout=5)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Read the image content from the response
+            image_content = response.content
+
+            # Create a PIL Image object from the image content
+            image = Image.open(BytesIO(image_content))
+
+            # Display the image
+            # image.show()
+        else:
+            print(f"Failed to download image. Status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        '''
